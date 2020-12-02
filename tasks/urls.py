f6feaebde30647e33b859cfc06206bd4f438b18f
luizasilveira <luizars7@hotmail.com>
2020-12-02 02:46:06 -0300

    tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
 
+from django.urls import path
 from . import views
+# from .views import TaskCreate, TaskList, TaskDelete, TaskUpdate
+from .views import getTasks
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('get', views.getTasks),
+    path('post', views.createTask),
+    path('delete', views.deleteTasks)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from tasks.models import Task
+from django.core import serializers
+from rest_framework.response import Response
+from tasks.serializer import TaskSerializer
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
+
+@api_view(["GET"])
+def get_tasks(request):
+    task = Task.objects.all()
+    task_json = serializers.serialize("json", task)
+    return HttpResponse(task_json, content_type="application/json")
 
 
-# Create your views here.
+@api_view(["POST"])
+def post_Task(request):
+    print(request)
+    infoJson = JSONParser().parse(request)
+    print(infoJson)
+    serializer_class = TaskSerializer(data=infoJson)
+    if serializer_class.is_valid():
+        serializer_class.save()
+        return JsonResponse(serializer_class.data, status=201)
+    return JsonResponse(serializer_class.errors, status=400)
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
+@api_view(["DELETE"])
+def delete_tasks(request):
+    Task.objects.all().delete()
+    return Response({"Deletet All tasks"})
